90b908fff573dbf36323ac8f304589cee88157f0
Agustin Feuerhake <agustin@platan.us>
2014-01-07 12:23:37 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Platdash::Application.config.secret_key_base = '9f477fa5086dd81e5e54e56ea1ca476f21e03bfca92a9de97beedcd3654ed66a819d1dbddb3ba66679b85f17c9989f28a5117814dd7ad900af67307ce226d958'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Platdash::Application.config.session_store :cookie_store, key: '_platdash_session'
