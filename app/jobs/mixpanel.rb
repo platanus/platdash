0981bbb22cc68e3c77e342f7889003875b2ecaf3
Agustin Feuerhake <agustin@platan.us>
2014-01-09 16:42:41 -0300

    corrects timezone for mixpanel data

--- app/jobs/mixpanel.rb
+++ app/jobs/mixpanel.rb
@@ -5,20 +5,20 @@ root = ::File.dirname(__FILE__)
 require ::File.join(root, "..", "..", 'lib', 'mixpanel_config')
 require ::File.join(root, "..", "..", 'lib', 'mixpanel_event_number')
  
-Dashing.scheduler.every '70s', :first_in => 10 do |job|
+Dashing.scheduler.every '30s', :first_in => 4 do |job|
   Dashing.send_event('qh_orders', {
     value: mixpanel_event_number(
       event_name: "Checkout Success",
-      interval:(Time.now.seconds_since_midnight/60 + Time.now.formatted_offset.to_i*60).to_i,
+      interval:(Time.now.in_time_zone("Santiago").seconds_since_midnight/60).to_i,
       unit:"minute")
   })
 end
 
-Dashing.scheduler.every '35s', :first_in => 6 do |job|
+Dashing.scheduler.every '15s', :first_in => 3 do |job|
   Dashing.send_event('qh_visits', {
     current: mixpanel_event_number(
       event_name: "Search",
-      interval:(Time.now.seconds_since_midnight/60 + Time.now.formatted_offset.to_i*60).to_i,
+      interval:(Time.now.in_time_zone("Santiago").seconds_since_midnight/60).to_i,
       unit:"minute")
   })
 end
