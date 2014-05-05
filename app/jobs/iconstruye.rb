708655a455279e47f6ba0ecb88140bdb16172632
Ignacio Baixas <iobaixas@gmail.com>
2014-05-05 16:59:47 -0400

    fix(widgets.Iconstruye): fixes refresh rate

--- app/jobs/iconstruye.rb
+++ app/jobs/iconstruye.rb
@@ -1,6 +1,6 @@
 require 'httpclient'
 
-Dashing.scheduler.every '2s', :first_in => 4 do
+Dashing.scheduler.every '30s', :first_in => 4 do
 
   client = HTTPClient.new
   status = client.get_content('http://iconstruye-api.platan.us/api/status.json')
