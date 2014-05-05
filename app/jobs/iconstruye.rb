ee1c8bc5cc3edcbd38d2160a0df569cf89b09a7b
Ignacio Baixas <iobaixas@gmail.com>
2014-05-05 16:44:16 -0400

    feat(widgets): adds iconstruye widget

---
+++ app/jobs/iconstruye.rb
@@ -0,0 +1,11 @@
+require 'httpclient'
+
+Dashing.scheduler.every '2s', :first_in => 4 do
+
+  client = HTTPClient.new
+  status = client.get_content('http://iconstruye-api.platan.us/api/status.json')
+  status = JSON.parse status
+
+  Dashing.send_event('iconstruye', status)
+
+end
