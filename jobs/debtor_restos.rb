e1c3ea04c77ad588420d57817ca04570038e8921
felipecamposclarke <felipecamposclarke@gmail.com>
2014-06-25 09:14:22 -0400

    changes the position and information of qh debtors

--- jobs/debtor_restos.rb
+++ jobs/debtor_restos.rb
@@ -2,7 +2,7 @@ require 'httpclient'
 
 SCHEDULER.every '20m', :first_in => 2 do
   client = HTTPClient.new
-  debtors = client.get_content('http://www.quehambre.cl/api/debtors?count=5')
+  debtors = client.get_content('http://www.quehambre.cl/api/debtors?count=16')
   debtors = JSON.parse debtors
   debtors = debtors['data']
   send_event('debtor_restos', {debtors: debtors})
