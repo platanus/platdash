9de6281aade42f0005843f1438ec1556671df051
Thomas Helleputte <thomas@platan.us>
2015-03-11 20:06:11 -0300

    feat():Madbill dashboard

--- jobs/madbill.rb
+++ jobs/madbill.rb
@@ -1,12 +1,11 @@
 require 'httpclient'
 
-SCHEDULER.every '5m', :first_in => 1 do
+SCHEDULER.every '5m', :first_in => 0 do
 
   client = HTTPClient.new
-  status = client.get_content('http://www.madbill.com/api/stats')
+  status = client.get_content('http://www.madbill.pow/api/stats')
   status = JSON.parse status
 
-  send_event('madbill', {
-    current: status['debtor_appuser_1w']})
+  send_event('madbill', status)
 
 end
