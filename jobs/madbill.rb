a659f87fde068fd2813f44cbe5539993fa41e911
Agustin Feuerhake <agustin@platan.us>
2015-03-12 08:38:35 -0300

    fix(): corrects madbill address

--- jobs/madbill.rb
+++ jobs/madbill.rb
@@ -3,7 +3,7 @@ require 'httpclient'
 SCHEDULER.every '5m', :first_in => 0 do
 
   client = HTTPClient.new
-  status = client.get_content('http://www.madbill.pow/api/stats')
+  status = client.get_content('http://www.madbill.com/api/stats')
   status = JSON.parse status
 
   send_event('madbill', status)
